863b4afb16b6891e798caa609df0e29efbfdb244
Hector <torrezhectorb@gmail.com>
2023-09-21 11:28:57 -0600

    3.18*

--- part3/Phonebook-backend/models/phonebook.js
+++ part3/Phonebook-backend/models/phonebook.js
@@ -20,10 +20,11 @@ const phonebookSchema = new mongoose.Schema({
 
 phonebookSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject._v
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
     }
 })
+
 
 module.exports = mongoose.model('Phonebook', phonebookSchema)
