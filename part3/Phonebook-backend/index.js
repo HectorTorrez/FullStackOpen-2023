cca414f7c7d40493e15fb0a600056587ce49f5f3
Hector <torrezhectorb@gmail.com>
2023-09-21 19:12:22 -0600

    3.22

--- part3/Phonebook-backend/index.js
+++ part3/Phonebook-backend/index.js
@@ -1,40 +1,38 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const cors = require('cors')
-const morgan = require('morgan');
+const morgan = require('morgan')
 const Phonebook = require('./models/phonebook')
-let list = require('./list.json');
+const list = require('./list.json')
 const { unkownEnpoint } = require('./middlewares/unkwonEnpoint.js')
 
-const PORT = process.env.PORT || 3001;
-app.disable('x-powered-by');
+const PORT = process.env.PORT || 3001
+app.disable('x-powered-by')
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :response-time ms - :body '))
 
-
-
 app.get('/api/persons', (req, res) => {
-    Phonebook.find({}).then(result => res.json(result))
-});
+  Phonebook.find({}).then(result => res.json(result))
+})
 
 app.get('/info', (req, res) => {
-    const time = new Date()
-    Phonebook.find({}).then(result => {
-        res.json(`Entries: ${result.length} Time: ${time}`)
-    })
-});
+  const time = new Date()
+  Phonebook.find({}).then(result => {
+    res.json(`Entries: ${result.length} Time: ${time}`)
+  })
+})
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    Phonebook.findById(id).then(item => {
-        res.json(item)
-    })
-});
+  const id = req.params.id
+  Phonebook.findById(id).then(item => {
+    res.json(item)
+  })
+})
 
 // const getRandomId = () => {
 //     const maxId = list.length > 0
@@ -45,68 +43,63 @@ app.get('/api/persons/:id', (req, res) => {
 // }
 
 app.post('/api/persons', async (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(404).json({
-            error: 'Name and number is missing'
-        })
-    }
-
-    const select = list.some(item => item.name.toLowerCase() === body.name)
-    if (select) {
-        return res.json({
-            error: 'name must be unique'
-        })
-    }
-
-    const newPerson = new Phonebook({
-        name: body.name,
-        number: body.number
+  if (!body.name || !body.number) {
+    return res.status(404).json({
+      error: 'Name and number is missing'
     })
+  }
 
-    newPerson.save().then(response => {
-        res.json(response)
-    }).catch(error => next(error))
+  const select = list.some(item => item.name.toLowerCase() === body.name)
+  if (select) {
+    return res.json({
+      error: 'name must be unique'
+    })
+  }
 
-});
+  const newPerson = new Phonebook({
+    name: body.name,
+    number: body.number
+  })
+
+  newPerson.save().then(response => {
+    res.json(response)
+  }).catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    const { name, number } = req.body
-    const newPerson = {
-        name: name,
-        number: number
-    }
+  const id = req.params.id
+  const { name, number } = req.body
 
-    Phonebook.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true, context: 'query' }).then(response => {
-        res.json(response)
-    })
-        .catch(error => next(error))
+  Phonebook.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true, context: 'query' }).then(response => {
+    res.json(response)
+  })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Phonebook.findByIdAndDelete(id).then(response => {
-        res.status(204).end()
-    })
-        .catch(error => next(error))
+  const id = req.params.id
+  Phonebook.findByIdAndDelete(id).then(response => {
+    res.status(204).end()
+  })
+    .catch(error => next(error))
 })
 
 app.use(unkownEnpoint)
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') {
-        res.status(400).send({ error: 'malformatted id' })
-    }
-    if (error.name === 'ValidationError') {
-        res.status(400).json({ error: error.message })
-    }
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    res.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    res.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server is running in port ${PORT}`)
-});
+  console.log(`Server is running in port ${PORT}`)
+})
--- part3/Phonebook-backend/models/phonebook.js
+++ part3/Phonebook-backend/models/phonebook.js
@@ -1,44 +1,41 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-
 const URL = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 
 mongoose.connect(URL)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    }).catch(error => {
-        console.log('Error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  }).catch(error => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        require: true
+  name: {
+    type: String,
+    minLength: 3,
+    require: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid number`
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function (v) {
-                return /^\d{2,3}-\d+$/.test(v)
-            },
-            message: props => `${props.value} is not a valid number`
-        },
-        require: [true, 'User phone number required']
-    }
+    require: [true, 'User phone number required']
+  }
 })
-
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-
 
 module.exports = mongoose.model('Phonebook', phonebookSchema)
