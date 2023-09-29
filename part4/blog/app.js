6d76bebee3379bfc7053b80018e0a8e499450bf0
Hector <torrezhectorb@gmail.com>
2023-09-29 10:27:30 -0600

    4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 
 const { info, error } = require('./utils/logger')
 const config = require('./utils/config')
-const notesRouter = require('./controllers/blogs')
+
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
 info('connecting to', config.MONGODB_URI)
@@ -17,6 +19,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
