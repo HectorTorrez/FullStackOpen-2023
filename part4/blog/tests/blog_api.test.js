595739ea1b451fed6530ee9077bc9cfd14946212
Hector <torrezhectorb@gmail.com>
2023-10-02 11:22:56 -0600

    4.23*

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -3,6 +3,9 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -19,6 +22,29 @@ describe('whe there is initially some blogs saved', () => {
 })
 
 describe('viewing a specific note', () => {
+  let token = null
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('1234', 10)
+    const user = await new User({ username: 'name', passwordHash }).save()
+    const userForToken = { username: user.username, id: user._id }
+    token = jwt.sign(userForToken, process.env.SECRET)
+    const newBlog = {
+      title: 'some blog',
+      author: 'some author',
+      url: 'https://www.example.com'
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    return token
+  })
   test('get id like a unique identifier', async () => {
     await api
       .get('/api/blogs')
@@ -32,6 +58,7 @@ describe('viewing a specific note', () => {
   })
 
   test('verify is the likes property is missing', async () => {
+    const blogsAtStart = await Blog.find({}).populate('user')
     const newBlog = {
       title: 'Eat',
       author: 'Torrez',
@@ -39,39 +66,53 @@ describe('viewing a specific note', () => {
     }
     await api
       .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
       .send(newBlog)
       .expect(201)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialState.length + 1)
-    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
+    const blogsAtEnd = await Blog.find({}).populate('user')
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length + 1)
+    expect(blogsAtEnd[0].likes).toBe(0)
   })
 
   test('verify if the title or url are mising', async () => {
+    const blogsAtStart = await Blog.find({}).populate('user')
     const newBlog = {
       author: 'Torrez Hector',
       likes: 450
     }
     await api
       .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
       .send(newBlog)
       .expect(400)
 
     const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialState.length)
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length)
   })
   test('succeeds with a status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
+    const blogsAtStart = await Blog.find({}).populate('user')
     const blogsToDelete = blogsAtStart[0]
 
     await api
       .delete(`/api/blogs/${blogsToDelete.id}`)
+      .set('Authorization', `hector ${token}`)
       .expect(204)
   })
 })
 
 describe('adition a new note', () => {
+  let token = null
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('1234', 10)
+    const user = await new User({ username: 'name', passwordHash }).save()
+    const userForToken = { username: user.username, id: user._id }
+    return (token = jwt.sign(userForToken, process.env.SECRET))
+  })
   test('POST/ create a new blog post', async () => {
+    const blogsAtStart = await Blog.find({}).populate('user')
     const newBlog = {
       title: 'Eat mango',
       author: 'Hector',
@@ -80,35 +121,86 @@ describe('adition a new note', () => {
     }
     await api
       .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
       .send(newBlog)
       .expect(201)
 
     const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialState.length + 1)
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length + 1)
   })
 })
 
 describe('deleting one note', () => {
-  test('success with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
-    const blogToDelete = blogsAtStart[0]
+  let token = null
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('1234', 10)
+    const user = await new User({ username: 'name', passwordHash }).save()
+    const userForToken = { username: user.username, id: user._id }
+    token = jwt.sign(userForToken, process.env.SECRET)
+    const newBlog = {
+      title: 'some blog',
+      author: 'some author',
+      url: 'https://www.example.com'
+    }
+
     await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
+      .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    return token
+  })
+  test('success with status code 204 if id is valid', async () => {
+    const blogsAtStart = await Blog.find({}).populate('user')
+    const blogsToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogsToDelete.id}`)
+      .set('Authorization', `hector ${token}`)
       .expect(204)
 
-    const blogs = await helper.blogsInDb()
-    expect(blogs).toHaveLength(helper.initialState.length - 1)
+    const blogs = await Blog.find({}).populate('user')
+    const blogsAtEnd = await Blog.find({}).populate('user')
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
     const author = blogs.map(b => b.author)
-    expect(author).not.toContain(blogToDelete.author)
+    expect(author).not.toContain(blogsToDelete.author)
   })
 })
 
 describe('updating one note', () => {
+  let token = null
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('1234', 10)
+    const user = await new User({ username: 'name', passwordHash }).save()
+    const userForToken = { username: user.username, id: user._id }
+    token = jwt.sign(userForToken, process.env.SECRET)
+    const newBlog = {
+      title: 'some blog',
+      author: 'some author',
+      url: 'https://www.example.com'
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `hector ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    return token
+  })
   test('success with status code 204 if id is valid', async () => {
-    const blogsAtStart = await helper.blogsInDb()
+    const blogsAtStart = await Blog.find({}).populate('user')
     const blogToUpdate = blogsAtStart[0]
     await api
       .patch(`/api/blogs/${blogToUpdate.id}`)
+      .set('Authorization', `hector ${token}`)
       .send({ likes: 300 })
       .expect(200)
     const blogs = await helper.blogsInDb()
